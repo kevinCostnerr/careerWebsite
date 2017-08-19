f910d70ab82b991a7accb801b1b9659073bcb094
jessicawiradinata <jessicawiradinata@gmail.com>
2017-08-19 13:44:53 +1000

    Material ui setup

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
 
 class App extends React.Component {
   render(){
     return (
     <div>
-      <h1>cyka blyat World</h1>
-      <h2>anjeng kau kimak lontong kuda cewek anjeng cowok anjeng</h2>
+      <MuiThemeProvider>
+        <AppBar
+          title="Title"
+          iconClassNameRight="muidocs-icon-navigation-expand-more"
+        />
+      </MuiThemeProvider>
+        <h1>cyka blyat World</h1>
+        <h2>anjeng kau kimak lontong kuda cewek anjeng cowok anjeng</h2>
     </div>
   )}
 }
