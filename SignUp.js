6077de06c9da19aa0150a8727a21ba67fc8c201d
stefanus yoshua <StefanusYoshua@stefanuss-MacBook-Pro.local>
2017-08-22 10:21:56 +1000

    Comments added

--- SignUp.js
+++ SignUp.js
@@ -1,9 +1,17 @@
+/**
+This page is the front end for the Sign Up view
+*/
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+// importing the MUIThemeProvider from material ui library
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+// importing the components from material ui
 import { FlatButton, ToolbarGroup, Toolbar, ToolbarTitle, Paper, TextField, RaisedButton } from 'material-ui';
+// import the routing
 import { HashLink as Link } from 'react-router-hash-link';
 
+// Styling for SignUp page
 const styles = {
     paper: {
         margin: 20,
@@ -26,10 +34,12 @@ export default class SignUp extends React.Component{
     return (
     <div>
         <MuiThemeProvider>
+            {/* Navbar*/}
             <Toolbar>
             <ToolbarGroup firstChild={true}>
                 <Link to="/"><FlatButton label="Career Website" primary={true} /></Link>
             </ToolbarGroup>
+            {/* routing for the NavBar*/}
             <ToolbarGroup lastChild={true}>
                 <Link to="/create-internship"><FlatButton label="Post" /></Link>
                 <Link to="/signup"><FlatButton label="Sign Up"  /></Link>
@@ -37,11 +47,14 @@ export default class SignUp extends React.Component{
             </ToolbarGroup>
             </Toolbar>
         </MuiThemeProvider>
+
         <MuiThemeProvider>
+            {/* Sign Up form */}
             <Paper style={styles.paper} zDepth={1}>
                 <h1>Sign Up</h1>
                 <TextField floatingLabelText="Email" style={styles.textField} />
                 <TextField floatingLabelText="Password" type="password" style={styles.textField} />
+
                 <RaisedButton label="Submit" primary={true} style={styles.submitBtn} />
             </Paper>
         </MuiThemeProvider>
--- server.js
+++ server.js
@@ -1,25 +1,37 @@
+/**
+This file is used to initialise the express server
+*/
+// initialising the variable needed by importing from library
 var path = require('path');
 var fs = require('fs');
 var express = require('express');
-
+// initialise the  variable that is going to be the port number
+var port = 8000;
+// initialise app variable using express
 var app = express();
 
+// setting the html as the view engine
 app.set('view engine', 'html');
+
+//get data from the fs
 app.engine('html', function(path, options, callbacks){
   fs.readFile(path, 'utf-8', callback);
 });
 
+// use express middleware to handles serving up the content from a directory
 app.use(express.static(__dirname));
 
+// adding the routes for the index.html
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+//catch error when the server is ran
 app.use(function(err, req, res, next){
   res.status(err.status || 500);
 });
 
-var port = 8000;
+//starting the server and 
 app.listen(port, function(){
   console.log('running at localhost:' + port);
 })
